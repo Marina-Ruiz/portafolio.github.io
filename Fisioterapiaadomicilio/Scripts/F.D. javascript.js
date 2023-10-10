44fdb403c2c8c77c06710f71a096f0671647d603
Marina-Ruiz <127426974+Marina-Ruiz@users.noreply.github.com>
2023-10-10 17:48:12 +0200

    Mi primer commit

---
+++ Fisioterapiaadomicilio/Scripts/F.D. javascript.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+
+
+// Creando las cookies
+document.cookie= "Primeracookie=datos";
+document.cookie="Segundacookie=usuario";
+
+//Cuando pinche los titulos que cambien de color
+
+let cambioColor= document.getElementById("inicio");
+
+inicio.addEventListener ("mouseover",encima);
+
+function encima(){
+	document.style.color="green";
+	
+}
