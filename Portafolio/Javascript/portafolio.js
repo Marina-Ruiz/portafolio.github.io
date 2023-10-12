6ee4c2cb97baf55083551a31aed2b36d8ed1bd76
Marina-Ruiz <127426974+Marina-Ruiz@users.noreply.github.com>
2023-10-12 14:24:21 +0200

    Add files via upload

---
+++ Portafolio/Javascript/portafolio.js
@@ -0,0 +1,11 @@
+<script>
+
+/*Aqui pongo el mensaje de bienvenida*/
+document.alert("Bienvenido a mi portafolio");
+
+/*Aqui pongo de la pestaña contacto un botón que nos lleve al watssap */ 
+
+const watssap=document.getElementById("botón");
+const.addEventListener(Onclick()=>);
+
+</script>
