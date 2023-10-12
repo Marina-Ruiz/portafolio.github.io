3976975e16df03b67a8343d80177d49bafdf4299
Marina-Ruiz <127426974+Marina-Ruiz@users.noreply.github.com>
2023-10-12 13:49:14 +0200

    Add files via upload

---
+++ Fruteria/Script/fruteria.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+
+// Aquí coloco el formulario de contacto del footer
+
+ /* <form name="formulario" action= "fruteria.html" method=" GET">
+Nombre y apellidos <input id="nombre" type="text" name="nombre">  
+Correo electrónico <input id="correo" type="text" name="correo">
+Número de teléfono <input id="teléfono" type="text" name="teléfono">
+<input id="submit" type="text" value="buscar">
+	
+</form> //
+
+// Aquí coloco las cookies
+function Creando cookies(){
+document.cookie="primeracookie= primera; expires: 2023-03-19;"
+document.cookie="Segundacookies=segunda; expires: 2023-03-19;"}
+
+/*Aquí creo un mensaje de bienvenida*/
+const bienvenida=window.alert("Bienvenido a fruterias Ruiz")};
+window.unonload= function()=>alert("Hasta pronto")};
+
+
+
+
+
+
